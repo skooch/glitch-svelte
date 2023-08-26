bf058ed2fc2bf53709df5f401ec27d8f477866ae
skooch <github@skooch.ws>
2023-08-27 00:28:36 +1000

    Fix: Use 0.0.0.0 as HOST

--- scripts/env.sh
+++ scripts/env.sh
@@ -14,7 +14,7 @@ export NODE_VERSION=v16
 export DEFAULT_NODE_DIR=/tmp/nvm/versions/node/v16/bin
 
 # Node
-# PROJECT_DOMAIN and HOST_ADDRESS is from Glitch
+# PROJECT_DOMAIN is from Glitch
 # We don't need to export PORT here because Glitch does
 export ORIGIN="$PROJECT_DOMAIN.glitch.me"
-export HOST=$HOST_ADDRESS
+export HOST="0.0.0.0"
